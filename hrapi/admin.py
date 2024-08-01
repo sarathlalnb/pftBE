395cb3b8c3ce2dd3c542980b44347e61fba382d6
Arunchandranvk <arunchandranvk84@gmail.com>
2024-08-01 11:53:15 +0530

    'latest'

--- hrapi/admin.py
+++ hrapi/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Performance_assign,Meeting
+from .models import *
 # Register your models here.
 admin.site.register(Performance_assign)
 admin.site.register(Meeting)
+admin.site.register(Employee)
+admin.site.register(CustomUser)
